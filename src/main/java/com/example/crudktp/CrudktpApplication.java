41ce1f878c30ca10a431536ceb71c5b5f8d64171
adilaroisa <adilaroisa688@gmail.com>
2026-03-12 20:54:22 +0700

    first commit

---
+++ src/main/java/com/example/crudktp/CrudktpApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudktpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudktpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudktp/CrudktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudktpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
